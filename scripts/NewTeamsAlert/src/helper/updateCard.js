4a6404ef40bb2fb4dfc3907107bbcb590d595756
nikunj.madhogaria <nikunj.madhogaria@sprinklr.com>
2024-06-21 16:47:29 +0530

    teams notifications review

--- scripts/NewTeamsAlert/src/helper/updateCard.js
+++ scripts/NewTeamsAlert/src/helper/updateCard.js
@@ -1,18 +1,19 @@
 // Update the card content with task details
 
 const updateCard = (cardTemplate, task, pending) => {
-  cardTemplate.body = cardTemplate.body.map(item => {
+  cardTemplate.body = cardTemplate.body.map((item) => {
     if (item.text) {
-      const shortFilePath = task.file.split('/').slice(-1).join('/');
-      item.text = item.text.replace('${username}', task.username)
-               .replace('${days}', task.days)
-               .replace('${type}', task.type.toUpperCase())
-               .replace('${title}', task.title)
-               .replace('${file}', (shortFilePath))
-               .replace('${message}', task.message)
-               .replace('${pendingTasks}', pending);
+      const shortFilePath = task.file.split("/").slice(-1).join("/");
+      item.text = item.text
+        .replace("${username}", task.username)
+        .replace("${days}", task.days)
+        .replace("${type}", task.type.toUpperCase())
+        .replace("${title}", task.title)
+        .replace("${file}", shortFilePath)
+        .replace("${message}", task.message)
+        .replace("${pendingTasks}", pending);
     }
-    
+
     // console.log(task.file)
     // item.items.forEach(item => {
     //   if (item.type === 'ColumnSet') {
@@ -36,18 +37,19 @@ const updateCard = (cardTemplate, task, pending) => {
     //   }
     // });
     return item;
-  }
-  );
-
-  cardTemplate.msteams.entities = cardTemplate.msteams.entities.map(entity => {
-    if (entity.text) {
-      entity.text = entity.text.replace('${username}', task.username);
-    }
-    return entity;
   });
+
+  //msTeams multi-word variables are written in camelCase
+  cardTemplate.msteams.entities = cardTemplate.msteams.entities.map(
+    (entity) => {
+      if (entity.text) {
+        entity.text = entity.text.replace("${username}", task.username);
+      }
+      return entity;
+    }
+  );
 
   return cardTemplate;
 };
 
 export default updateCard;
-
